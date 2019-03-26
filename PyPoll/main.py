576bba7e0f753bfa04e5556987e1181e3c284745
Showc <26051742+faraic@users.noreply.github.com>
2019-03-26 09:26:26 -0600

    dynamic print for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -43,31 +43,27 @@ candidates_percentage_dict=dict(zip(unique_candidates,candidate_percentage))
 
 #create a dictionary that has key-value pairs arranged by candidates numbers
 sorted_candidates_numbers_dict=(dict(sorted(candidates_numbers_dict.items(), key=lambda x:x[1], reverse=True)))
-print(sorted_candidates_numbers_dict)
 
 #Extract an ordered unique candidate list from the preceeding dictionary
 ordered_unique_candidates=list(sorted_candidates_numbers_dict.keys())
-print(ordered_unique_candidates)
+
 
 #mapping using candidates numbers dictionary to print the Key paring the value with the highest number of observations
 winners_name=max(candidates_numbers_dict, key=lambda x: candidates_numbers_dict[x])
 
-
+#Printing results for all candidates in the ordered unique candidates list/Making it a dynamic print
 print('''Election Results
 --------------------------
 Total Votes: %d
---------------------------
-Khan: %.3f%% (%d) 
-Correy: %.3f%% (%d)
-Li: %.3f%% (%d)
-O'Tooley: %.3f%% (%d)
----------------------------
+--------------------------'''%(count))
+
+for x in ordered_unique_candidates:
+   print('%s: %.3f%% (%d)'%(x,candidates_percentage_dict[x],candidates_numbers_dict[x]) )  
+
+print('''---------------------------
 Winner: %s
 ---------------------------
-''' %(count,candidates_percentage_dict['Khan'],candidates_numbers_dict['Khan']
-    ,candidates_percentage_dict['Correy'],candidates_numbers_dict['Correy']
-    ,candidates_percentage_dict['Li'],candidates_numbers_dict['Li']
-    ,candidates_percentage_dict["O'Tooley"],candidates_numbers_dict["O'Tooley"],winners_name))
+''' %(winners_name))
 
 with open('PyPoll/pypoll.txt', 'w') as text:
     text.write('''Election Results
