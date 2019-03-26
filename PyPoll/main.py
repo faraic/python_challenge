b07fe03a3242e42130aa51b73657e62e31e6d073
Showc <26051742+faraic@users.noreply.github.com>
2019-03-26 10:02:55 -0600

    incorporating the dynamic print into writting the text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,8 @@
-#loading the needed modules
+# loading the needed modules
 import os
 import csv
 
-#Setting file path for the CSV file
+# Setting file path for the CSV file
 
 election_csv = os.path.join("PyPoll", "Resources","election_data.csv")
 
@@ -19,16 +19,16 @@ with open(election_csv, newline="") as csvfile:
  # skipping the header   
     csv_header = next(csvreader)
 
- # reading through each row
+ # Reading through each row
     for row in csvreader:
 
-      #counting the number of rows   
+      # Counting the number of rows   
       count = count + 1     
    
       # Creating a list with cantidates by appending each value in the candidate column as a string
       candidates.append(row[2])
 
-#Creatig a list with candidate unique names
+# Creatig a list with candidate unique names
 unique_candidates=list(set(candidates))
 
 # Adding number of votes and '%'s to the two lists initialized in the beginning using the unique candidates names
@@ -37,21 +37,21 @@ for x in unique_candidates:
       candidate_numbers.append(candidates.count(x))
       candidate_percentage.append(candidates.count(x)/count*100)
 
-#creating numbers and percentages dictionaries with the candidates as keys
+# Creating numbers and percentages dictionaries with the candidates as keys
 candidates_numbers_dict=dict(zip(unique_candidates,candidate_numbers))
 candidates_percentage_dict=dict(zip(unique_candidates,candidate_percentage))
 
-#create a dictionary that has key-value pairs arranged by candidates numbers
+# Create a dictionary that has key-value pairs arranged by candidates numbers
 sorted_candidates_numbers_dict=(dict(sorted(candidates_numbers_dict.items(), key=lambda x:x[1], reverse=True)))
 
-#Extract an ordered unique candidate list from the preceeding dictionary
+# Extract an ordered unique candidate list from the preceeding dictionary
 ordered_unique_candidates=list(sorted_candidates_numbers_dict.keys())
 
 
-#mapping using candidates numbers dictionary to print the Key paring the value with the highest number of observations
+# Mapping using candidates numbers dictionary to print the Key paring the value with the highest number of observations
 winners_name=max(candidates_numbers_dict, key=lambda x: candidates_numbers_dict[x])
 
-#Printing results for all candidates in the ordered unique candidates list/Making it a dynamic print
+# Printing results for all candidates in the ordered unique candidates list/Making it a dynamic print
 print('''Election Results
 --------------------------
 Total Votes: %d
@@ -65,19 +65,21 @@ Winner: %s
 ---------------------------
 ''' %(winners_name))
 
+# Creating a text file for the results
 with open('PyPoll/pypoll.txt', 'w') as text:
     text.write('''Election Results
 --------------------------
 Total Votes: %d
---------------------------
-Khan: %.3f%% (%d) 
-Correy: %.3f%% (%d)
-Li: %.3f%% (%d)
-O'Tooley: %.3f%% (%d)
----------------------------
+--------------------------\n'''%(count))
+
+# Appending the candidates results to the text file
+for x in ordered_unique_candidates:
+   with open('PyPoll/pypoll.txt', 'a') as text:
+    text.write('%s: %.3f%% (%d)\n'%(x,candidates_percentage_dict[x],candidates_numbers_dict[x]) )  
+
+# Appending the winner results to the text file
+with open('PyPoll/pypoll.txt', 'a') as text:
+   text.write('''---------------------------
 Winner: %s
 ---------------------------
-''' %(count,candidates_percentage_dict['Khan'],candidates_numbers_dict['Khan']
-    ,candidates_percentage_dict['Correy'],candidates_numbers_dict['Correy']
-    ,candidates_percentage_dict['Li'],candidates_numbers_dict['Li']
-    ,candidates_percentage_dict["O'Tooley"],candidates_numbers_dict["O'Tooley"],winners_name))
+''' %(winners_name))
